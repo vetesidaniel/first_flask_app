753c16b0201008cf438c65f44af74a934aff89e1
Vetési Dani <vetesi.daniel998@gmail.com>
2021-11-03 22:46:24 +0100

    Implemented new post creation function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 
@@ -20,6 +20,7 @@ def get_post(post_id):
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Schonherz2021'
 
 
 @app.route('/')
@@ -37,6 +38,25 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
